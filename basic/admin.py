798f8c9a1007b49e2f20e6d033b375b17b9f9962
Sharath N <sharath0628@gmail.com>
2015-03-20 23:24:12 +0530

    changes to the models

---
+++ basic/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from basic.models import SubQuestion,Subject,QuestionPaper,MainQuestion
+# Register your models here.
+
+admin.site.register(SubQuestion)
+admin.site.register(Subject)
+admin.site.register(QuestionPaper)
+admin.site.register(MainQuestion)
