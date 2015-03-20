bbcef687f285f14da68c1e0f78b24215aa75f12c
Sharath N <sharath0628@gmail.com>
2015-03-20 23:47:02 +0530

    models

--- qpgen/basic/models.py
+++ qpgen/basic/models.py
@@ -6,16 +6,20 @@ class SubQuestion(models.Model):
 	question_text=models.CharField(max_length=200)
 	question_marks=models.IntegerField(default=0)
 	subject=models.ForeignKey('Subject')
-	#image=
+	image=models.ImageField()
 	question_difficulty=models.CharField(max_length=10)
 	#unit
 	section=models.CharField(max_length=5)
+	def __str__(self):
+		return self.question_text
 
 class Subject(models.Model):
 	subject_name=models.CharField(max_length=40)
 	subject_code=models.CharField(max_length=10)
 	semester=models.IntegerField(default=0)
 	branch=models.CharField(max_length=10)
+	def __str__(self):
+		return self.subject_name
 
 class QuestionPaper(models.Model):
 	subject=models.ForeignKey('Subject')
@@ -23,6 +27,10 @@ class QuestionPaper(models.Model):
 	max_marks=models.IntegerField(default=0)
 	date_of_exam=models.DateTimeField()
 	teacher_name=models.CharField(max_length=30)
+	def __str__(self):
+		return self.subject.subject_name
 
 class MainQuestion(models.Model):
 	sub_question=models.ForeignKey('SubQuestion')
+	def __str__(self):
+		return self.sub_question.question_text
