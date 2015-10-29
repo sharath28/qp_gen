2eeadb9b0bca0cf6136ce95786a0119a46ceeef2
Amjad Ali <amjad.4@gmail.com>
2015-10-29 20:56:33 +0530

    Modified models

--- basic/models.py
+++ basic/models.py
@@ -1,4 +1,12 @@
 from django.db import models
+from flufl.enum import IntEnum
+
+class DifficultTypeEnum(IntEnum):
+	unknown = 0
+	easy = 1
+	medium = 2
+	hard = 3
+
 
 class Subject(models.Model):
 	name		= models.CharField(max_length=200)
@@ -10,36 +18,41 @@ class Subject(models.Model):
 		return self.name
 
 
-class SubQuestion(models.Model):
+class Question(models.Model):
+	DIFFICULTY_TYPES = [ (int(enum_choice), enum_choice.name) for enum_choice in DifficultyTypeEnum ]
+
 	text		  = models.CharField(max_length=500)
 	marks		  = models.IntegerField(default=0)
-	subject 	  = models.ForeignKey(Subject)
-	image 		  = models.ImageField(max_length=100)
-	difficulty	  = models.CharField(max_length=10)
-	section 	  = models.CharField(max_length=5)
+	subject 	  = models.ForeignKey('Subject')
+	difficulty	  = models.IntegerField(default=int(DifficultyTypeEnum.unknown), choices=DIFFICULTY_TYPES)
+	unit		  = models.IntegerField()
 
 	def __str__(self):
 		return self.subject.name
 
 
+class QuestionImage(models.Model):
+	question	= models.ForeignKey('Question')
+	caption		= models.CharField(max_length=200, blank=True)
+	image		= models.ImageField(upload_to='question_images')
+
+
 class MainQuestion(models.Model):
-	subquestions = models.ManyToManyField(SubQuestion)
+	sub_questions = models.ManyToManyField('Question')
 
 
 class QuestionPaper(models.Model):
 	university		= models.CharField(max_length=200)
 	subject 		= models.ForeignKey(Subject)
-	mainquestions	= models.ManyToManyField(MainQuestion)
+	main_questions	= models.ManyToManyField('MainQuestion')
 	max_marks		= models.IntegerField(default=0)
 	date_of_exam	= models.DateField()
 	teacher_name	= models.CharField(max_length=30)
+	created_at			= models.DateTimeField(auto_now_add=True, null=False, editable=False)
+	updated_at			= models.DateTimeField(auto_now=True, null=False, editable=False)
 
 	def __str__(self):
 		semester = self.subject.semester
 		subject_name = self.subject.name
 
-		return "#{} [Sem {}] {}".format(self.pk,semester,subject_name) 
-
-	def __str__(self):
-		
-		return ""
+		return "#{} [Sem {}] {}".format(self.pk,semester,subject_name)
